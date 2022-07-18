b6151aa1a8aa45049a402a97dcfc8298c3d77c78
Falcao1303 <lucasjesus22@hotmail.com>
2022-07-18 20:05:53 -0300

    alteracao rotas

--- apiPHP/api-laravel/routes/api.php
+++ apiPHP/api-laravel/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/reset', [OperationsController::class, 'resetRegisters']);
+Route::get('/balance', [OperationsController::class, 'getRegisters']);
+Route::post('/event', [OperationsController::class, 'event']);
