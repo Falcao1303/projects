9196f39c90357aeee54cf2f7652f4f66d7248052
Falcao1303 <lucasjesus22@hotmail.com>
2022-07-21 18:12:11 -0300

    requisicao get da api pokedex

--- pokedex/js/controller.js
+++ pokedex/js/controller.js
@@ -8,14 +8,28 @@ pokedexController.$inject = ['$scope', '$http', '$rootScope', '$location', '$win
 function pokedexController($scope, $http, $rootScope, $location, $window,httpdService) {
     var $injector = angular.injector();
     var vm = $scope;
+    vm.getList = getList;
     iniciarController();
 
+
     function iniciarController(){
-        vm.loginForm={
-            email:'',
-            password:'',
-        }
+        getList();
     }
+
+    function getList(){
+    vm.teste = $http({
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
+        }).then(function successCallback(response) {
+            vm.list = response.data.results;
+            console.log(response);
+        }
+        );
+
+    }    
+    
+
+
 
 
 }
