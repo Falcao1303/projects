007f1f51ed3fd730adec883890fa85f5e8143b44
Falcao1303 <lucasjesus22@hotmail.com>
2022-07-25 22:40:35 -0300

    ajustando

--- pokedex/js/controller.js
+++ pokedex/js/controller.js
@@ -1,11 +1,11 @@
-var app = angular.module('pokedex',['httpdService']);
+var app = angular.module('pokedex',[]);
 
 app.controller('pokedexController',pokedexController)
 
 
-pokedexController.$inject = ['$scope', '$http', '$rootScope', '$location', '$window','httpdService'];
+pokedexController.$inject = ['$scope', '$http', '$rootScope', '$location', '$window'];
 
-function pokedexController($scope, $http, $rootScope, $location, $window,httpdService) {
+function pokedexController($scope, $http, $rootScope, $location, $window) {
     var $injector = angular.injector();
     var vm = $scope;
     vm.getList = getList;
@@ -35,27 +35,28 @@ function pokedexController($scope, $http, $rootScope, $location, $window,httpdSe
 
     function nextPage(){
         vm.next = true;
+        vm.proxOffset = vm.offset + 40;
             let offset =  vm.offset + 20;
-            console.log('https://pokeapi.co/api/v2/pokemon/?offset='+ offset + '&limit=20');
             vm.searchNext = $http({
                 method: 'GET',
                 url: 'https://pokeapi.co/api/v2/pokemon/?offset='+ offset + '&limit=20'
             }).then(function successCallback(response) {
                 vm.list = response.data.results;
-                vm.total = response.data.count;
-                vm.totalpages = Math.ceil(vm.total/20);
-                for(let i = 1; i <= 898; i++){
+                for(let i = vm.offset+20; i <= vm.proxOffset; i++){
+                    // o i tem que ser no maximo até 20 mas as imagens tem que pegar a partir do offset
                     vm.searchImages = $http({
                         method: 'GET',
                         url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+ i +'.png'
                     }).then(function (response){
-                        angular.extend(vm.list[i-1],{image: response.config.url});
+                        console.log("next",vm.list[i]);
+                        angular.extend(vm.list[i],{image: response.config.url});
+                  
                     });
+                  
                 }
                 vm.next = false;
                 vm.page = vm.page + 1;
-                vm.offset = vm.offset + 20;
-                console.log("success",vm.offset);
+                // vm.offset = vm.offset + 20;
             }
          );
     }
@@ -64,21 +65,20 @@ function pokedexController($scope, $http, $rootScope, $location, $window,httpdSe
         vm.next = false;
         vm.previous = true;
         let offset = vm.offset - 20;
-        vm.page = vm.page - 1;
             vm.searchPrevious = $http({
                 method: 'GET',
                 url: 'https://pokeapi.co/api/v2/pokemon/?offset='+ offset + '&limit=20'
             }).then(function (response) {
                 vm.list = response.data.results;
-                for(let i = 1; i <= 898; i-1){
-                    vm.searchImages = $http({
-                        method: 'GET',
-                        url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+ i +'.png'
-                    }).then(function (response){
-                        angular.extend(vm.list[i-1],{image: response.config.url});
-                    });
+                // for(let image = vm.offset; image <= 898; image-1){
+                //     vm.searchImages = $http({
+                //         method: 'GET',
+                //         url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+ image +'.png'
+                //     }).then(function (response){
+                //         angular.extend(vm.list[i-1],{image: response.config.url});
+                //     });
     
-                }
+                // }
                 vm.next = false;
                 vm.page = vm.page - 1;
                 vm.offset = vm.offset - 20;
@@ -91,18 +91,16 @@ function pokedexController($scope, $http, $rootScope, $location, $window,httpdSe
                     method: 'GET',
                     url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
                 }).then(function successCallback(response) {
-                    console.log(response);
                     vm.list = response.data.results;
                     vm.total = response.data.count;
                     vm.totalpages = Math.ceil(vm.total/20);
-                    for(let i = 1; i <= 898; i++){
+                    for(let i = 1; i <= 20; i++){
                         vm.searchImages = $http({
                             method: 'GET',
                             url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+ i +'.png'
                         }).then(function (response){
                             angular.extend(vm.list[i-1],{image: response.config.url});
                         });
-
                     }
                 }
              );
