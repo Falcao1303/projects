ac4f7265c0bc996e32a00c9070c20808561363d7
Falcao1303 <lucasjesus22@hotmail.com>
2022-11-13 14:16:55 -0300

    terminando primeira parte do typescript

--- typescriptproject/dist/js/models/negociacao.js
+++ typescriptproject/dist/js/models/negociacao.js
@@ -1,11 +1,12 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
     get data() {
-        return this._data;
+        const data = new Date(this._data.getTime());
+        return data;
     }
     get quantidade() {
         return this._quantidade;
