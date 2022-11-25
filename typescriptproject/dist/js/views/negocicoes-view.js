375c028a6247a1bb355ff60f14d6352c4008fa56
Falcao1303 <lucasjesus22@hotmail.com>
2022-11-24 21:05:13 -0300

    ajustes tabela

---
+++ typescriptproject/dist/js/views/negocicoes-view.js
@@ -0,0 +1,22 @@
+export class NegociacoesView {
+    constructor(seletor) {
+    }
+    template() {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+            <tfoot>
+            </tfoot>
+        </table>
+        `;
+    }
+}
