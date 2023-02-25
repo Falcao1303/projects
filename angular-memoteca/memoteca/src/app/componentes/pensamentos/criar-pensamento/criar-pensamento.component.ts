c0eaebbede4dcea1ba70c85b0ed8c2370b6c45d4
Falcao1303 <lucasjesus22@hotmail.com>
2023-02-24 22:09:29 -0300

    feat:ajustando models formulario

--- angular-memoteca/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-memoteca/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo : 'Pensamento 1',
+    autoria : 'Autor 1',
+    modelo : ' ',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  criarPensamento()  {
+    alert("teste" + this.pensamento.conteudo)
+  }
+
+  cancelar() {
+    alert("teste cancelado")
+  }
 }
