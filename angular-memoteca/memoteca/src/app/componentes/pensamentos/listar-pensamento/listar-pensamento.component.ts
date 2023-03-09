ca5869abc5abce072ad3a30d2d00b37d96826384
Falcao1303 <lucasjesus22@hotmail.com>
2023-03-08 23:04:24 -0300

    feat:ajustando adicionar pensamento

--- angular-memoteca/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular-memoteca/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [];
   constructor() { }
 
   ngOnInit(): void {
