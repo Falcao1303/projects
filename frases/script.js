114c3875b82226fad66cf442bc9d92606ec37948
Falcao1303 <lucasjesus22@hotmail.com>
2023-09-04 21:02:16 -0300

    ajustando responsividade e logos

--- frases/script.js
+++ frases/script.js
@@ -6,16 +6,16 @@ appController.$inject = ['$scope', '$http', '$rootScope', '$location', '$window'
 
 function appController($scope, $http, $rootScope, $location, $window) {
     $scope.mensagem = [
-        "Ponta Grossa, cidade das águas, das flores, e do povo acolhedor.",
-        "Ponta Grossa, terra de gente bonita, de sorriso aberto, e de coração generoso.",
-        "Ponta Grossa, cidade de oportunidades, de sonhos realizados, e de futuro promissor.",
-        "Ponta Grossa, terra de história, de cultura, e de belas paisagens.",
-        "Ponta Grossa, cidade de alegria, de esperança, e de amor.",
-        "Ponta Grossa, cidade de gente feliz, de gente sorridente, e de gente que gosta de viver.",
-        "Ponta Grossa, cidade de gente hospitaleira, de gente amiga, e de gente que gosta de compartilhar.",
-        "Ponta Grossa, cidade de gente trabalhadora, de gente esforçada, e de gente que não desiste dos seus sonhos.",
-        "Ponta Grossa, cidade de gente orgulhosa, de gente que ama a sua cidade, e de gente que quer sempre o melhor para ela.",
-        "Ponta Grossa, cidade de gente que faz a diferença, de gente que inspira, e de gente que faz o mundo um lugar melhor."
+        "Ponta Grossa, cidade acolhedora.",
+        "Terra de gente bonita e generosa.",
+        "Cidade de oportunidades e futuro promissor.",
+        "História, cultura e belas paisagens.",
+        "Cidade de alegria, esperança e amor.",
+        "Gente feliz, sorridente e que ama viver.",
+        "Gente hospitaleira, amiga e compartilhadora.",
+        "Gente trabalhadora e determinada.",
+        "Gente orgulhosa, que ama sua cidade e busca o melhor.",
+        "Gente que faz a diferença e torna o mundo melhor."
     ];
     $scope.mensagensExibidas = []; // Array para acompanhar as mensagens exibidas
     $scope.mensagem_gerada = "";
